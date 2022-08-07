690e04c67a92ce88429ca9dbe0d6cd0457f7cd86
tikmart <t.martirosyan012@gmail.com>
2022-08-07 14:57:41 +0400

    4th commit

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -10,6 +10,7 @@ public class Dog extends Animal {
     public void dogDescription(){
 
         System.out.println(dog.getDescription("brown", 4,true));
+        System.out.println(dog.getDescription("red", 1,true));
     }
 
 }
